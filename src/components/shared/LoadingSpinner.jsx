2b3c4e861d1bf223987ebc26624c540721bff03f
tylarochele4-netizen <tylarochele4@gmail.com>
2026-04-21 21:01:32 +0300

    Add LoadingSpinner component for loading indication

---
+++ src/components/shared/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+function LoadingSpinner({ text = "Loading..." }) {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <div className="spinner">🔄</div> {/* You can add a CSS spinner later */}
+            <p>{text}</p>
+        </div>
+    );
+}
+export default LoadingSpinner;
