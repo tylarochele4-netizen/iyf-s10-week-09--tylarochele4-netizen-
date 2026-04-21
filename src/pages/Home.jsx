9d1512192f75b431d80dffcb859ebb96a87c7e28
tylarochele4-netizen <tylarochele4@gmail.com>
2026-04-21 20:52:34 +0300

    Add Home component for the homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>🏠 Welcome to CommunityHub</h2>
+      <p>This is the homepage. Navigate to the Posts tab to see real-time data fetching in action!</p>
+    </div>
+  );
+}
+export default Home;
